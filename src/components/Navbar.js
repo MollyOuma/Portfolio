331b48914bca6b6998305ccfdc1d26a48dcfe209
MollyOuma <princessmolly991@gmail.com>
2024-07-25 16:44:10 +0300

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
+const scrollToSection = (sectionId) => {
+  const element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -8,22 +15,27 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = (sectionId) => {
+    scrollToSection(sectionId);
+    setIsOpen(false); // Close the menu when a link is clicked
+  };
+
   return (
-    <nav className="fixed w-full z-40 top-0 bg-white border overflow-hidden">
+    <nav className={`fixed w-full z-40 top-0 border bg-white overflow-hidden`}>
       <div className="container mx-auto px-4 flex justify-between items-center py-6 md:px-16">
         <div className="flex items-center">
           <img src="../logo2.png" alt="Logo" className="h-8 w-8 mr-2" />
-          <div className="text-2xl font-bold text-blue-500">
+          <div className={`text-2xl font-bold text-blue-500`}>
             Sylvester
           </div>
         </div>
-        <div className="hidden md:flex space-x-10 ">
-          <a href="#introduction" className="text-lg text-gray-900 hover:text-blue-500">Home</a>
-          <a href="#about" className="text-lg text-gray-900 hover:text-blue-500">About</a>
-          <a href="#services" className="text-lg text-gray-900 hover:text-blue-500">Services</a>
-          <a href="#portfolio" className="text-lg text-gray-900 hover:text-blue-500">Portfolio</a>
-          <a href="#resume" className="text-lg text-gray-900 hover:text-blue-500">Resume</a>
-          <a href="#contact" className="text-lg text-gray-900 hover:text-blue-500">Contact</a>
+        <div className="hidden md:flex space-x-10">
+          <button onClick={() => scrollToSection('introduction')} className="text-lg text-gray-900 hover:text-blue-500">Home</button>
+          <button onClick={() => scrollToSection('about')} className="text-lg text-gray-900 hover:text-blue-500">About</button>
+          <button onClick={() => scrollToSection('services')} className="text-lg text-gray-900 hover:text-blue-500">Services</button>
+          <button onClick={() => scrollToSection('portfolio')} className="text-lg text-gray-900 hover:text-blue-500">Portfolio</button>
+          <button onClick={() => scrollToSection('resume')} className="text-lg text-gray-900 hover:text-blue-500">Resume</button>
+          <button onClick={() => scrollToSection('contact')} className="text-lg text-gray-900 hover:text-blue-500">Contact</button>
         </div>
         <div className="md:hidden">
           <button onClick={toggleMenu} className="focus:outline-none">
@@ -37,14 +49,14 @@ const Navbar = () => {
         <motion.div 
           initial={{ opacity: 0, height: 0 }}
           animate={{ opacity: 1, height: 'auto' }}
-          className="md:hidden"
+          className="md:hidden bg-color"
         >
-          <a href="#introduction" className="block py-2 px-4 text-gray-700  hover:text-gray-700">Home</a>
-          <a href="#about" className="block py-2 px-4 text-gray-700 hover:text-gray-700">About</a>
-          <a href="#services" className="block py-2 px-4 text-gray-700 hover:text-gray-700">Services</a>
-          <a href="#portfolio" className="block py-2 px-4 text-gray-700 hover:text-gray-700">Portfolio</a>
-          <a href="#resume" className="block py-2 px-4 text-gray-700 hover:text-gray-700">Resume</a>
-          <a href="#contact" className="block py-2 px-4 text-gray-700 hover:text-gray-700">Contact</a>
+          <button onClick={() => handleLinkClick('introduction')} className="block py-2 px-4 text-white hover:text-gray-300">Home</button>
+          <button onClick={() => handleLinkClick('about')} className="block py-2 px-4 text-white hover:text-gray-300">About</button>
+          <button onClick={() => handleLinkClick('services')} className="block py-2 px-4 text-white hover:text-gray-300">Services</button>
+          <button onClick={() => handleLinkClick('portfolio')} className="block py-2 px-4 text-white hover:text-gray-300">Portfolio</button>
+          <button onClick={() => handleLinkClick('resume')} className="block py-2 px-4 text-white hover:text-gray-300">Resume</button>
+          <button onClick={() => handleLinkClick('contact')} className="block py-2 px-4 text-white hover:text-gray-300">Contact</button>
         </motion.div>
       )}
     </nav>
